065cb940602ee1bf8489377f93ca1f80c1a59a90
dcsiona@gmail.com <dcsiona@gmail.com>
2024-03-21 13:59:03 -0400

    another commit

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,17 +5,43 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        let newNode = new SinglyLinkedNode(val);
+
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this.length;
+
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+        if (this.length === 0) return null;
+
+        let removedNode = this.head;
+
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        this.length--;
+        return removedNode.value;
+
         // Write your hypothesis on the time complexity of this method here
     }
 
